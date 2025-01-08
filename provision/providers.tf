6486f1707f93269c45d1e2c20d4728003fca6774
iamnagasatya <n.nagasatyasai@gmail.com>
2025-01-08 20:44:24 +0530

    update provider

--- provision/providers.tf
+++ provision/providers.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_providers {
+    ansible = {
+      source = "ansible/ansible"
+      version = "1.3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-provider "ansible" {}
+provider "ansible" {
+  # Configuration options
+}
